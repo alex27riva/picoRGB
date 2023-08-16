c5d29f7b2ea2cc658718336c311f5968e32ec577
Alessandro Riva <alex27riva@gmail.com>
2023-08-16 15:22:16 +0200

    Rename library Updated example

--- RGB_example.py
+++ RGB_example.py
@@ -1,18 +1,21 @@
-from pyRGB import RGBLed
+from picoRGB import RGBLed
 
-myRGB = RGBLed(15,14,13,RGBLed.anode)
+led = RGBLed(15,14,13,RGBLed.cathode)
 
 """
-myRGB = RGBLed(#Red pin,#Green pin, #Blue pin,#RGB Led type -anode or cathode)
-myRGB.off() -              Turn off all colors
-myRGB.set(120,50,75)       Set color Set(#Red Led,#Green Led,#Blue Led)  
-myRGB.slowSet(10,200,195)  Set color slowly SlowSet(#Red Led,#Green Led,#Blue Led,#delay - optional)
-myRGB.show()               Show last color values
-myRGB.white()              Set RGB led to white (255,255,255)
-myRGB.yellow()             Set RGB led to yellow (255,255,0)
-myRGB.magenta()            Set RGB led to magenta (255,0,255)
-myRGB.cyan()               Set RGB led to cyan (0,255,255)  
+led = RGBLed(#Red pin,#Green pin, #Blue pin,#RGB Led type -anode or cathode)
+led.off() -              Turn off all colors
+led.set(120,50,75)       Set color Set(#Red Led,#Green Led,#Blue Led)  
+led.slowSet(10,200,195)  Set color slowly SlowSet(#Red Led,#Green Led,#Blue Led,#delay - optional)
+led.show()               Show last color values
+led.red()              Set RGB led to red (255,0,0)
+led.green()              Set RGB led to green (0,255,0)
+led.blue()              Set RGB led to blue (0,0,255)
+led.white()              Set RGB led to white (255,255,255)
+led.yellow()             Set RGB led to yellow (255,255,0)
+led.magenta()            Set RGB led to magenta (255,0,255)
+led.cyan()               Set RGB led to cyan (0,255,255)  
 """
 while True:
-    myRGB.slowSet(0,0,0)
-    myRGB.slowSet(255,255,255)
+    led.slowSet(0,0,0)
+    led.slowSet(255,255,255)
